8cb9ec7be15b3072a07506097415079d20854e8e
Victor Correal <victor.correal@bsc.es>
2024-08-21 22:59:03 +0200

    Add bash env variables expansion

--- app/abstract_runner.py
+++ app/abstract_runner.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-
+import os
 
 
 class AbstractRunner:
@@ -8,6 +8,16 @@ class AbstractRunner:
     @staticmethod
     def empty_recipe():
         pass
+
+    def __post_init__(self):
+        for v in self.__dict__.values():
+            if isinstance(v, str) and v.startswith("$"):
+                print(">Expanding bash env var", v)
+                env_val = os.getenv(v[1:])
+                if env_val is None:
+                    raise Exception("Unable to find env var", v, env_val)
+                else:
+                    v = env_val
 
     def check_parameters(self):
         raise Exception("UNDEFINED RUN")
