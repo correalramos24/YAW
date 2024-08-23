8bee063d1c87d355fd81fc8afbbb6b7044f07b6c
Victor Correal ThinkPad <correal.ramos@gmail.com>
2024-08-23 17:46:10 +0200

    Update bash runner example

--- example/bash_runner/env.sh
+++
@@ -1,5 +0,0 @@
-
-
-echo "Loading environment"
-#printenv | head -n 1
-export MY_VAR=33
--- example/bash_runner/my_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "I'm a test script running at $PWD!"
-echo "Arguments: $@"
-echo "My_var_from_env: $MY_VAR"
-
-echo "Ending scrip!"
-exit 123
