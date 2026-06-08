8d68877be0e219727762376e055834e7bd6898c3
Victor Correal Ramos <victor.correal@migx.ch>
2026-06-08 11:41:41 +0200

    Clean before uv

--- yaw/nemo/__init__.py
+++
@@ -1,4 +0,0 @@
-from .AbstractNemo5Runner import AbstractNemo5Runner
-from .NEMO5Runner import NEMO5Runner
-from .TestNEMO5_runner import TestNEMO5Runner
-from .NEMO5XIOSRunner import NEMO5XIOSRunner
--- yaw/yaw_ascii.py
+++
@@ -1,9 +0,0 @@
-
-logo_ascii = \
-"""
-Y     Y     A     W           W
- Y   Y     A A     W         W
-  Y Y     A   A     W   W   W
-   Y     AAAAAAA     W W W W 
-   Y    A       A     W   W  
-"""
